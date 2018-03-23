ca0425d0f7439d365a6a3ec26d92a0021764958d
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-23 11:01:15 +0100

    individual digits

--- IndividualDigits/Program.cs
+++ IndividualDigits/Program.cs
@@ -8,8 +8,31 @@ namespace IndividualDigits
 {
     class Program
     {
+        private static void PrintDigit(int number, int numberOfDigits)
+        {
+            int digit = number / numberOfDigits;
+            int modulo = number % numberOfDigits;
+            Console.Write(String.Format("{0} ", digit));
+            if (numberOfDigits == 1)
+            {
+                return;
+            }
+            numberOfDigits /= 10;
+            PrintDigit(modulo, numberOfDigits);
+        }
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Input any number");
+                string input = Console.ReadLine();
+                int numberOfDigits = input.Trim().Length;
+                int number = Convert.ToInt32(input);
+                PrintDigit(number, Convert.ToInt32(Math.Pow(10, numberOfDigits-1)));
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
