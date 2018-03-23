c350bb5c29bfc44fb92ff5128ecdb19bdd1dc599
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-23 12:35:43 +0100

    count digits recursivly

---
+++ CountDigits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDigits
+{
+    class Program
+    {
+        private static void CountDigits(int initialNumber, int currentNumber, int numberOfDigits)
+        {
+            if (currentNumber / 10 < 1)
+            {
+                Console.WriteLine(String.Format("The number {0} cointains number of digits : {1}", initialNumber, numberOfDigits));
+                return;
+            }
+
+            CountDigits(initialNumber, currentNumber / 10, ++numberOfDigits);
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input any number");
+                int number = Convert.ToInt32(Console.ReadLine());
+                CountDigits(number, number, 1);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
