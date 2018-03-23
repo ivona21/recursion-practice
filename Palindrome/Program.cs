3c7c25176f0684f7c2fb3b503f22f81012aae84a
Ivona Malekovic <ivonamalekovic@hotmail.com>
2018-03-23 13:21:35 +0100

    palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        private static void IsPalindrome(string word, int counter, int limit)
+        {
+            if (word.Length == 1)
+            {
+                Console.WriteLine("String is only one character long, so ofcourse it is Palindrome.");
+                return;
+            }
+
+            if (counter > limit)
+            {
+                Console.WriteLine("The string is Palindrome.");
+                return;
+            }
+
+            if (word[counter].Equals(word[word.Length - 1 - counter]))
+            {
+                IsPalindrome(word, ++counter, limit);
+            }
+            else
+            {
+                Console.WriteLine("The string is not Palindrome.");
+                return;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input a string");
+                string word = Console.ReadLine();
+                IsPalindrome(word, 0, word.Length / 2 + 1);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
